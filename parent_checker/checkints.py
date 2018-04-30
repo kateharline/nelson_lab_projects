437e3468b77c915cd4f4e1d461550eebc3961a12
Kate Harline <kharline@wustl.edu>
2018-04-30 15:28:23 -0400

    cleaning introgressions entries for rs range lists

--- parent_checker/checkints.py
+++ parent_checker/checkints.py
@@ -3,15 +3,28 @@ import os
 
 import checkparents as cp
 
-def convert_to_rs(c_and_pos):
-    '''
-    convert chromosome and start position values into rs# in tassel format
-    :param c_and_pos: dataframe of chromosome (int) and position (obj) values
-    :return: one dataframe column with rs#s (str)
-    '''
-    # todo decide best concat
+def clean_strs(list):
+    return [''.join(entry.split()) for entry in list]
 
-    return
+def convert_to_rs_ranges(chromosomes, starts, ends):
+    '''
+    make rs-like values to search snp space with
+    :param chromosome: list of ints chromosome number
+    :param start: list of strs that clean to ints start of introgression
+    :param end: list of strs clean to ints start of introgression
+    :return: list of lists of strs rs#s
+    '''
+    rs_s = []
+    starts = clean_strs(starts)
+    ends = clean_strs(ends)
+
+    for i in range(chromosomes):
+        rs = []
+        for pos in range(starts[i], ends[i]):
+            rs.append('S'+str(chromosomes[i])+'_'+str(pos))
+        rs_s.append(rs)
+
+    return rs_s
 
 def clean_ints(ints_df):
     '''
@@ -20,10 +33,11 @@ def clean_ints(ints_df):
     :return: new df with only useful info
     '''
     useful_ints = ints_df.loc[:, 'line':'end']
-    useful_ints['start'] = convert_to_rs(useful_ints[['chromosome','start']])
-    useful_ints['end'] = convert_to_rs(useful_ints['chromosome', 'end'])
+    useful_ints['rs_strings'] = convert_to_rs_range(useful_ints[['chromosome','start','end']])
 
     # todo convert
+    # dict snp : val
+    # do lookups of snps
 
     return useful_ints
 
@@ -41,10 +55,11 @@ def check_ints(lines, founders, ints):
 
 def main():
     # open snp data
-    founders, lines = cp.open_founders_and_nils('10NN_CU_with_founders_full.hmp.txt', '10NN_CU_with_founders_full.hmp.txt')
+    # todo uncomment to run the script for analysis
+    # founders, lines = cp.open_founders_and_nils('10NN_CU_with_founders_full.hmp.txt', '10NN_CU_with_founders_full.hmp.txt')
 
     os.chdir('/Users/kateharline/Desktop/nelson_lab/parent_checker/inputs')
-    ints_df = pd.read_csv('10NN_introgressions.txt', sep="\t")
+    ints_df = pd.read_csv('int_test.txt', sep="\t")
 
     os.chdir('/Users/kateharline/Desktop/nelson_lab/parent_checker/outputs')
     ints = clean_ints(ints_df)
